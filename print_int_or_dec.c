c894613af14260843206930d27a98fb0c1a451cd
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-11-26 22:52:31 +0000

    function for print %d or %i

---
+++ print_int_or_dec.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ *print_int_or_dec - prints a integer or decimal
+ *@args: argument to print
+ *Return: number of charcters to printed
+ */
+int print_int_or_dec(va_list args)
+{
+	int n = va_arg(args, int);
+	int num = n, last = n % 10, digit, pos = 1;
+	int i = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+		last = -last;
+		i++;
+	}
+
+	while (num / 10 != 0)
+	{
+		pos *= / 10;
+	}
+
+	num = n < 0 ? -n : n;
+	while (pos > 0)
+	{
+		digit = num / exp;
+		_putchar(digit + '0');
+		num = num - (digit * pos);
+		pos = pos / 10;
+		i++;
+	}
+
+	_putchar(last + '0');
+	i++;
+
+	return (i);
+}
